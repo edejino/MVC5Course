e4bbcd046327bf9b95cabd48218f1c238232ad0d
Jino <jino@edetw.com>
2016-03-21 13:59:56 +0800

    練習「從資料庫更新模型」的注意事項

--- MVC5Course/Models/Product.cs
+++ MVC5Course/Models/Product.cs
@@ -9,10 +9,10 @@
 
 namespace MVC5Course.Models
 {
-	using System;
-	using System.Collections.Generic;
-	using System.ComponentModel.DataAnnotations;
-	public partial class Product
+    using System;
+    using System.Collections.Generic;
+    
+    public partial class Product
     {
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2214:DoNotCallOverridableMethodsInConstructors")]
         public Product()
@@ -20,16 +20,11 @@ namespace MVC5Course.Models
             this.OrderLine = new HashSet<OrderLine>();
         }
     
-		[Required]
         public int ProductId { get; set; }
-		[Required]
-		public string ProductName { get; set; }
-		[Required]
-		public Nullable<decimal> Price { get; set; }
-		[Required]
-		public Nullable<bool> Active { get; set; }
-		[Required]
-		public Nullable<decimal> Stock { get; set; }
+        public string ProductName { get; set; }
+        public Nullable<decimal> Price { get; set; }
+        public Nullable<bool> Active { get; set; }
+        public Nullable<decimal> Stock { get; set; }
     
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
         public virtual ICollection<OrderLine> OrderLine { get; set; }

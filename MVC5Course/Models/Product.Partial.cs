772145f28e1195f447b098396eb2cce7d8bd2804
Jino <jino@edetw.com>
2016-03-21 16:44:33 +0800

    練習實作「模型驗證」 ( IValidableObject )

--- MVC5Course/Models/Product.Partial.cs
+++ MVC5Course/Models/Product.Partial.cs
@@ -3,13 +3,25 @@ namespace MVC5Course.Models
     using System;
     using System.Collections.Generic;
     using System.ComponentModel.DataAnnotations;
-    
-    [MetadataType(typeof(ProductMetaData))]
-    public partial class Product
-    {
-    }
-    
-    public partial class ProductMetaData
+
+	[MetadataType(typeof(ProductMetaData))]
+	public partial class Product : IValidatableObject
+	{
+		public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+		{
+			if(this.Stock > 10 && this.Price < 100)
+			{
+				yield return new ValidationResult("價格設定錯誤", new string[] { "Price" });
+			}
+
+			if(this.Stock < 5)
+			{
+				yield return new ValidationResult("庫存量過低，無法新增商品", new string[] { "Stock" });
+			}
+		}
+	}
+
+	public partial class ProductMetaData
     {
         [Required]
         public int ProductId { get; set; }

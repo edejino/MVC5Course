befbbe0debb4422cbead07ca19147d50d2931fc4
Jino <jino@edetw.com>
2016-04-06 14:34:14 +0800

    請將 /Home/Test 套用 HandleError 屬性，在不同的例外情況下顯示不同的 View 頁面

--- MVC5Course/Controllers/HomeController.cs
+++ MVC5Course/Controllers/HomeController.cs
@@ -15,9 +15,17 @@ namespace MVC5Course.Controllers
 			return View();
         }
 
-        public ActionResult About()
+		[HandleError(ExceptionType = typeof(InvalidOperationException), View = "Error2")]
+		public ActionResult About(string name)
         {
-            ViewBag.Message = "Your application description page.";
+			if (String.IsNullOrEmpty(name))
+			{
+				throw new ArgumentException("參數錯誤");
+			}
+
+			throw new InvalidOperationException("操作錯誤");
+
+			ViewBag.Message = "Your application description page.";
 
             return View();
         }
